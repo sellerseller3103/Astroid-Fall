36fdde0d34b6e228af28d2bbdad8fe55e67818c6
Daniel Hernandez <danielhernandez@Daniels-MacBook-Pro.local>
2017-02-24 17:16:13 -0600

    Trying to make orange move

--- JS/Astroid Fall.js
+++ JS/Astroid Fall.js
@@ -1,1 +1,15 @@
 // start below
+$(document).ready(function() {
+	$(document).keydown(function(b) {
+		
+		if(b.keyCode === 37){
+		$("#orange").animate({left: "-1px"}, "slow");
+		
+		}
+		if(b.keyCode === 39) {
+		$("#orange").animate({rightt: "-1px"}, "slow");
+		}
+	
+	})
+
+});
